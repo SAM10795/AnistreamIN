43bebbe223ec2f811c4baa54d29c009025b3289f
SAM10795 <silversoulsam@gmail.com>
2022-07-17 15:46:22 +0530

    temp update

--- menu_json.js
+++ menu_json.js
@@ -2,116 +2,110 @@
   "menus": [
     {
       "date": "15 July",
-      "menu": [
-        {
-          "Breakfast": [
-            "Sweet Khaman",
-            "Omelette",
-            "Boiled Eggs",
-            "Fruit",
-            "Milk",
-            "Tea",
-            "Coffee",
-            "Bread Butter Jam",
-            "Moong Dal"
-          ],
-          "Lunch": [
-            "Mutter Paneer",
-            "Dudhi Chana",
-            "Roti",
-            "Methi Dal",
-            "Plain Rice",
-            "Papad",
-            "Salad"
-          ],
-          "Dinner": [
-            "Flower Mutter",
-            "Malai Kofta",
-            "Roti",
-            "Steamed Rice",
-            "Khichdi",
-            "Curd",
-            "Papad",
-            "Salad"
-          ]
-        }
-      ]
+      "menu": {
+        "Breakfast": [
+          "Sweet Khaman",
+          "Omelette",
+          "Boiled Eggs",
+          "Fruit",
+          "Milk",
+          "Tea",
+          "Coffee",
+          "Bread Butter Jam",
+          "Moong Dal"
+        ],
+        "Lunch": [
+          "Mutter Paneer",
+          "Dudhi Chana",
+          "Roti",
+          "Methi Dal",
+          "Plain Rice",
+          "Papad",
+          "Salad"
+        ],
+        "Dinner": [
+          "Flower Mutter",
+          "Malai Kofta",
+          "Roti",
+          "Steamed Rice",
+          "Khichdi",
+          "Curd",
+          "Papad",
+          "Salad"
+        ]
+      }
     },
     {
       "date": "16 July",
-      "menu": [
-        {
-          "Breakfast": [
-            "Upma",
-            "Omelette",
-            "Boiled Eggs",
-            "Fruit",
-            "Milk",
-            "Tea",
-            "Coffee",
-            "Bread Butter Jam",
-            "Moong Dal"
-          ],
-          "Lunch": [
-            "Mix Veg",
-            "Dudhi Chana",
-            "Paratha",
-            "Methi Dal",
-            "Plain Rice",
-            "Papad",
-            "Salad"
-          ],
-          "Dinner": [
-            "Manchurian",
-            "Schezwan",
-            "Roti",
-            "Steamed Rice",
-            "Khichdi",
-            "Curd",
-            "Papad",
-            "Salad"
-          ]
-        }
-      ]
+      "menu": {
+        "Breakfast": [
+          "Upma",
+          "Omelette",
+          "Boiled Eggs",
+          "Fruit",
+          "Milk",
+          "Tea",
+          "Coffee",
+          "Bread Butter Jam",
+          "Moong Dal"
+        ],
+        "Lunch": [
+          "Mix Veg",
+          "Dudhi Chana",
+          "Paratha",
+          "Methi Dal",
+          "Plain Rice",
+          "Papad",
+          "Salad"
+        ],
+        "Dinner": [
+          "Manchurian",
+          "Schezwan",
+          "Roti",
+          "Steamed Rice",
+          "Khichdi",
+          "Curd",
+          "Papad",
+          "Salad"
+        ]
+      }
     },
     {
       "date": "17 July",
-      "menu": [
-        {
-          "Breakfast": [
-            "Dosa",
-            "Chutney",
-            "Omelette",
-            "Boiled Eggs",
-            "Banana",
-            "Milk",
-            "Tea",
-            "Coffee",
-            "Bread Butter Jam"
-          ],
-          "Lunch": [
-            "Veg Tadka",
-            "Baigan Masala",
-            "Paratha",
-            "Methi Dal",
-            "Plain Rice",
-            "Papad",
-            "Salad",
-            "Chhaas"
-          ],
-          "Dinner": [
-            "Shahi Paneer",
-            "Aloo Jeera",
-            "Roti",
-            "Steamed Rice",
-            "Khichdi",
-            "Curd",
-            "Papad",
-            "Salad",
-            "Ice Cream"
-          ]
-        }
-      ]
+      "menu": {
+        "Breakfast": [
+          "Dosa",
+          "Chutney",
+          "Omelette",
+          "Boiled Eggs",
+          "Banana",
+          "Milk",
+          "Tea",
+          "Coffee",
+          "Bread Butter Jam"
+        ],
+        "Lunch": [
+          "Veg Tadka",
+          "Baigan Masala",
+          "Paratha",
+          "Methi Dal",
+          "Plain Rice",
+          "Papad",
+          "Salad",
+          "Chhaas"
+        ],
+        "Dinner": [
+          "Shahi Paneer",
+          "Aloo Jeera",
+          "Roti",
+          "Steamed Rice",
+          "Khichdi",
+          "Curd",
+          "Papad",
+          "Salad",
+          "Ice Cream"
+        ]
+      }
     }
   ]
 }
